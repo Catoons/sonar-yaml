d09ec5162f5718f7303518c44d696905380e020b
Alden <73409576+Catoons@users.noreply.github.com>
2024-07-10 11:05:41 -0400

    Update ForbiddenValueCheck.java

--- src/main/java/com/github/sbaudoin/sonar/plugins/yaml/checks/ForbiddenValueCheck.java
+++ src/main/java/com/github/sbaudoin/sonar/plugins/yaml/checks/ForbiddenValueCheck.java
@@ -61,7 +61,7 @@ public class ForbiddenValueCheck extends ForbiddenCheck {
                 Matcher m = forbiddenValuePattern.matcher(((ScalarToken)t3).getValue());
                 if (m.find()) {
                     // Report new error
-                    addViolation("Forbidden value " + key + " found in " + keyName, t);
+                    addViolation("Forbidden value " + value + " found in " + keyName, t);
                 }
             }
         }
